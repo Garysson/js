0cb5d01f1b830073996f2c16ef5df95b660cdb2a
Grigoriy <cashwiz@ukr.net>
2020-02-16 16:01:53 +0200

    add index js file with script

---
+++ index.js
@@ -0,0 +1,7 @@
+let myName = 'Gary';
+let mySurname = 'Slipchenko';
+let myAge = '32';
+let messege = 'Hello!';
+let infirmation = messege + ' ' + 'My name is'  + ' ' + myName + ' ' + 'my surname is' + ' ' + mySurname + ' ' +  'My age is' + ' ' + myAge;
+
+alert( infirmation );
