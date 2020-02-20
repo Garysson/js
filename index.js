600a74a03d85e388d34430ec9c38a4c8edaab058
Grigoriy <cashwiz@ukr.net>
2020-02-20 11:27:40 +0200

    create new js with quations about age and alcohol

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-let myName = 'Gary';
-let mySurname = 'Slipchenko';
-let myAge = '32';
-let messege = 'Hello!';
-let infirmation = messege + ' ' + 'My name is'  + ' ' + myName + ' ' + 'my surname is' + ' ' + mySurname + ' ' +  'My age is' + ' ' + myAge;
+"use strict";
 
-alert( infirmation );
+let age = prompt('Ваш возраст', '');
+let drink = confirm('Алкаголь употребляем?');
+
+if (age >= 40 && drink == true) {
+    alert ('Не злоупотребляйте');
+    } else if (age < 18 && drink == true) {
+    alert ('Ты что?! Маме расскажу!');
+    } else if (age >=18 && drink == true) {
+    alert ('Только водку с пивом не мешай...');
+    } else {
+    alert ('Так держать!');
+    };
