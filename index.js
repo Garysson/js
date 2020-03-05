978857f78ba5162e4c95a997d938e6056784cdff
Grigoriy <cashwiz@ukr.net>
2020-03-05 10:24:51 +0200

    change function

--- index.js
+++ index.js
@@ -4,112 +4,44 @@ let a = prompt('Radius of circle','');
 let b = prompt('Enter first number','');
 let q = prompt('Enter second number', '');
 let c = prompt('Please neter one of mathematic operator + - * /', '');
-let sumNumber = sum();
-let minusNumber = minus();
-let divisionNumber = division();
-let multiplicationNumber = multiplication();
 const PI = 3.1415;
 
-function isItNumberFirst () {
-    let c = +a;
-    let b = c + 'a';
-
-    if ( b === 'NaNa') {
-        return console.log('Please enter radius of ciercle value as a number');
-    }
+function areaCircle () {
+    return console.log('Площадь круга состaвляет ' + (a*b)*(a*b));
 }
 
-function areaCircle (a, b) {
-    return (a*b)*(a*b);
-}
+function areaPerimeter () {
+    const b = 2 * PI;
 
-function areaPerimeter (a, b) {
-    return (a)*(2*b);
-}
-
-function getUsersRadius (dataUserRadius) {
-    let x = a;
-    let y = PI;
-
-    return dataUserRadius(x, y);
-}
-
-function isItNumberSecond () {
-    let c = +b;
-    let d = c + 'a';
-
-    let w = +q;
-    let e = w + 'a';
-
-    if ( d === 'NaNa') {
-        return console.log('Please enter first number value as a number');
-    } else if ( e === 'NaNa') {
-        return console.log('Please enter second value as a number');
-    }
-}
-
-
-
-function sum (x, y) {
-    const c = +b;
-    const e = +q;
-
-    return c + e;
+    return console.log('Длина окружности состaвляет ' + +a * b);
 }
 
 function avarage () {
     const c = 2;
-    let b = sum();
+    const y = b + q;
 
-    return b / c;
+    return console.log('Среднее арифметическое двух чисел составляет ' + b / c);
 }
 
-function minus (x, y) {
-    const c = +b;
-    const e = +q;
-
-    return c - e;
-}
-
-
-function division (x, y) {
-    const c = +b;
-    const e = +q;
-
-    return c / e;
-}
-
-function multiplication (x, y) {
-    const c = +b;
-    const e = +q;
-
-    return c * e;
-}
-
-function getResult() {
-    let z = c;
+function calc(a, w, e) {
+    const v = b;
+    const n = q;
+    const l = c;
 
     if (c === '+') {
-        return console.log('Сумма чисел равна ' + sumNumber);
+        return console.log(+b + +q);
     } else if (c === '-') {
-        return console.log('Вычетание чисел равно ' + minusNumber);
-    } else if (c === '/') {
-        return console.log('Деление чисел равно ' + divisionNumber);
+        return console.log(+b - +q);
     } else if (c === '*') {
-        return console.log('Умножение чисел равно ' + multiplicationNumber);
+        return console.log(+b * +q);
+    } else if (c === '/') {
+        return console.log(+b / +q);
     } else {
-        return console.log('Please neter one of mathematic operator + - * /')
+        return alert('Put one of this meanings + - / *')
     }
 }
 
-isItNumberFirst();
-
-isItNumberSecond();
-
-console.log('Площадь круга состaвляет ' + getUsersRadius(areaCircle) + ' метров квадратных');
-
-console.log('Длина окружности состaвляет ' + getUsersRadius(areaPerimeter) + ' метров');
-
-console.log('Среднее арифметическое двух чисел составляет ' + avarage(sum));
-
-getResult();
+areaCircle();
+avarage();
+areaPerimeter();
+calc();
