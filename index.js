159fa0b17cb02ffe21b635fb80035c744134f5f3
Grigoriy <cashwiz@ukr.net>
2020-03-29 15:14:23 +0300

    add new four functions

--- index.js
+++ index.js
@@ -1,36 +1,69 @@
 "use strict";
 
-function areaCircle (a) {
-    let b = Math.pow(a, 2) * Math.PI;
+const myObjeckt = {
+    name: 'Vasya',
+    age: 24,
+    status: 'student'
+};
 
-    return b;
-}
+let arr = [1, 2];
 
-function areaPerimeter (a) {
-    return (a) * (2 * Math.PI);
-}
+function copy(target, origin) {
+    let newArr = [];
 
-function avarage (x, y) {
-    return (x + y) / 2;
-}
-
-function getResult(a, b, c) {
-
-    if (c === '+') {
-        return a + b;
-    } if (c === '-') {
-        return a - b;
-    } if (c === '/') {
-        return a / b;
-    } if (c === '*') {
-        return a * b;
+    for (let prop in origin) {
+        newArr[prop] = origin[prop];
     }
+
+    return newArr;
 }
 
-console.log('Площадь круга состaвляет ' + areaCircle(3) + ' м2');
-console.log('Длина окружности состaвляет ' + areaPerimeter(10) + ' м.');
-console.log('Среднее арифметическое двух чисел составляет ' + avarage(4, 10));
-console.log('Сумма чисел равна ' + getResult(2, 2, '+'));
-console.log('Вычетание чисел равно ' + getResult(2, 2, '-'));
-console.log('Деление чисел равно ' + getResult(2, 2, '/'));
-console.log('Умножение чисел равно ' + getResult(2, 2, '*'));
+console.log(copy(arr, myObjeckt));
+
+function sumAll () {
+    let a = 0;
+
+    for (let i = 0; i <= arguments.length - 1; i ++) {
+        a += i
+    }
+
+    return a
+}
+
+console.log(sumAll(1, 2, 3, 4));
+
+function getNumberOfEven(n) {
+    let a = n + '';
+    let count = 0;
+
+    for (let i = 0; i <= a.length - 1; i++) {
+        if (a[i] % 2 === 0) {
+            count += 1;
+        }
+    }
+
+    return count
+}
+
+console.log(getNumberOfEven(223344));
+
+Math.min.apply(Math, arr)
+
+function mathMinMax() {
+    let arr = [];
+    let brr = ['',''];
+
+    for (let prop in arguments) {
+        arr[prop] = arguments[prop];
+    }
+
+    let a = Math.min(...arr);
+    let b = Math.max(...arr);
+
+    brr['0'] = a;
+    brr['1'] = b;
+
+    return brr
+}
+
+console.log(mathMinMax(3, 4, 8, 555, 7, 2, 3, -100, 2, 3, 5));
