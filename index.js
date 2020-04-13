3f6f3f14e7be47e6d75a4f090a008a5716039b03
Grigoriy <cashwiz@ukr.net>
2020-04-13 11:51:20 +0300

    create two function whit 5 square and 1 link hash

--- index.js
+++ index.js
@@ -1,43 +1,105 @@
 "use strict";
 
-// Task 2
+Array.from(document.querySelectorAll('.square')).forEach(function(el){
+    el.addEventListener('click', function(el){
 
-"use strict";
+        let target = el.target;
+        if (target.tagName === 'BUTTON') return;
 
-let link = (((decodeURIComponent((window.location).hash).replace('#?','&')).split('&utm_')).join()).split(',').filter(Boolean);
-
-let arr = [];
-
-link.forEach((el) => {
-    let a = {};
-        a[el.split('=')[0]] = el.split('=')[1]
-    arr.push(a)
+        let b = (this.innerHTML).toString();
+        let numb = b.match(/\d/g);
+        numb = numb.join('');
+        let num = +(numb) + 1;
+        let str = `${num}<div class="wrap-button"><button class="plus">+</button><button class="minus">-</button></div>`;
+        this.innerHTML = str;
+    })
 })
 
-function createTable(arr) {
-    let str = '<table>'
+let getClosest = function (elem, selector) {
 
-    for (let i = 0; i < arr.length ; i++) {
-        str += '<tr>';
-
-        for (var prop in arr[i]) {
-            str += `<td>${prop}</td><td>${arr[i][prop]}</td>`
-        }
-
-        str += '</tr>';
+    if (!Element.prototype.matches) {
+        Element.prototype.matches =
+            Element.prototype.matchesSelector ||
+            Element.prototype.mozMatchesSelector ||
+            Element.prototype.msMatchesSelector ||
+            Element.prototype.oMatchesSelector ||
+            Element.prototype.webkitMatchesSelector ||
+            function(s) {
+                let matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                    i = matches.length;
+                while (--i >= 0 && matches.item(i) !== this) {}
+                return i > -1;
+            };
     }
 
-    str += '</table>'
+    for ( ; elem && elem !== document; elem = elem.parentNode ) {
+        if ( elem.matches( selector ) ) return elem;
+    }
+    return null;
+};
 
-    return str
-}
+document.addEventListener('click', function (event) {
+
+    if ( event.target.classList.contains('minus') ) {
+        let parent = getClosest(event.target, '.square');
+        let b = (parent.innerHTML).toString();
+        let numb = b.match(/\d/g);
+        numb = numb.join('');
+
+        if (+numb === 0) {
+            return
+        };
+
+        let num = +(numb) - 1;
+        let str = `${num}<div class="wrap-button"><button class="plus">+</button><button class="minus">-</button></div>`;
+        parent.innerHTML = str;
+    }
+
+}, false);
+
+document.addEventListener('click', function (event) {
+
+    if ( event.target.classList.contains('plus') ) {
+        let parent = getClosest(event.target, '.square');
+        let b = (parent.innerHTML).toString();
+        let numb = b.match(/\d/g);
+        numb = numb.join('');
+        let num = +(numb) + 1;
+        let str = `${num}<div class="wrap-button"><button class="plus">+</button><button class="minus">-</button></div>`;
+        parent.innerHTML = str;
+    }
+
+}, false);
+
+document.getElementById('link-for-text').addEventListener('click', function(el){
+    let link = (((decodeURIComponent((window.location).hash).replace('#?','&')).split('&utm_')).join()).split(',').filter(Boolean);
+    let arr = [];
+
+    link.forEach((el) => {
+        let a = {};
+    a[el.split('=')[0]] = el.split('=')[1];
+    arr.push(a);
+})
+
+    function createTable(arr) {
+        let str = '<table>';
+
+        for (let i = 0; i < arr.length; i++) {
+            str += '<tr>';
+            for (var prop in arr[i]) {
+                str += `<td>${prop}</td><td>${arr[i][prop]}</td>`;
+            }
+            str += '</tr>';
+        }
+        str += '</table>';
+        return str;
+    }
+
+    this.text = 'Click to create Table';
+
+    document.getElementById('myTable').innerHTML = createTable(arr);
+})
 
 document.getElementById('reset').onclick = function() {
     location.reload();
 }
-
-let div = document.createElement('div');
-
-div.innerHTML = createTable(arr);
-
-document.body.append(div);
